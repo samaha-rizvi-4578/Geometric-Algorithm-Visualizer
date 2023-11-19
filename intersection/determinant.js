06b43500d65bece5492c4d8ae2ec6c4bdbf13c33
Laiba Meer <k214897@nu.edu.pk>
2023-11-20 02:52:54 +0500

    Update determinant.js

--- intersection/determinant.js
+++ intersection/determinant.js
@@ -1,82 +1,5 @@
-const line1 = generateRandomLinePoints();
-const line2 = generateRandomLinePoints();
-visualizeLines(line1, line2);
-
-
-const result = doLinesIntersect(line1, line2);
-console.log("Do the lines intersect?", result);
-
-// Function to calculate the determinant of a 2x2 matrix
-function determinant(x1, y1, x2, y2) {
-    return x1 * y2 - x2 * y1;
-}
-
-// Function to check if two line segments intersect
-function doLinesIntersect(line1, line2) {
-    const [x1, y1] = line1[0];
-    const [x2, y2] = line1[1];
-    const [x3, y3] = line2[0];
-    const [x4, y4] = line2[1];
-
-    const det1 = determinant(x1 - x3, y1 - y3, x4 - x3, y4 - y3);
-    const det2 = determinant(x2 - x3, y2 - y3, x4 - x3, y4 - y3);
-    const det3 = determinant(x3 - x1, y3 - y1, x2 - x1, y2 - y1);
-    const det4 = determinant(x4 - x1, y4 - y1, x2 - x1, y2 - y1);
-
-    return (det1 * det2 < 0) && (det3 * det4 < 0);
-}
-// Function to generate random points for a line
-function generateRandomLinePoints() {
-    const x1 = Math.random() * 10;
-    const y1 = Math.random() * 10;
-    const x2 = Math.random() * 10;
-    const y2 = Math.random() * 10;
-    return [[x1, y1], [x2, y2]];
-}
-
-// Function to visualize the lines using D3.js
-function visualizeLines(lines) {
-    const svg = d3.select("#visualization");
-
-    // Clear existing lines
-    svg.selectAll("line").remove();
-
-    // Visualize each line in the array
-    lines.forEach((line, index) => {
-        svg.append("line")
-            .attr("x1", line[0][0] * 50)
-            .attr("y1", line[0][1] * 50)
-            .attr("x2", line[1][0] * 50)
-            .attr("y2", line[1][1] * 50)
-            .attr("stroke", `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`);
-    });
-    const result = checkIntersections(lines);
-    document.getElementById("result").innerText = "Do the lines intersect? " + result;
-
-}
-function clearVisualization() {
-    const svg = d3.select("#visualization");
-    svg.selectAll("line").remove();
-}
-
-//for displaying message about interaction
-function checkIntersections(lines) {
-    const numLines = lines.length;
-
-    // Check for intersections among all pairs of lines
-    for (let i = 0; i < numLines - 1; i++) {
-        for (let j = i + 1; j < numLines; j++) {
-            if (doLinesIntersect(lines[i], lines[j])) {
-                return true; // Intersection found
-            }
-        }
-    }
-
-    return false; // No intersections found
-}
-
 function generateLines() {
-    const numLines = parseInt(document.getElementById("numLines").value, 10);
+    const numLines = 3;
     const lines = [];
 
     for (let i = 0; i < numLines; i++) {
@@ -85,6 +8,95 @@ function generateLines() {
     }
 
     visualizeLines(lines);
+    const intersectionPoint = findIntersectionPoint(lines);
+    if (intersectionPoint) {
+        document.getElementById("result").innerText = "Intersection Point: " + intersectionPoint;
+    } else {
+        document.getElementById("result").innerText = "No Intersection Point";
+    }
 }
 
+function visualizeLines(lines) {
+    const svg = d3.select("#visualization");
 
+    // Clear previous lines
+    svg.selectAll("*").remove();
+
+    lines.forEach(line => {
+        // Visualize line
+        svg.append("line")
+            .attr("x1", line[0][0])
+            .attr("y1", line[0][1])
+            .attr("x2", line[1][0])
+            .attr("y2", line[1][1])
+            .attr("stroke", getRandomColor());
+    });
+}
+
+function generateRandomLinePoints() {
+    const x1 = Math.random() * 300;
+    const y1 = Math.random() * 300;
+    const x2 = Math.random() * 300;
+    const y2 = Math.random() * 300;
+    return [[x1, y1], [x2, y2]];
+}
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Function to check if two line segments intersect using parametric approach
+function findIntersectionPoint(lines) {
+    const numLines = lines.length;
+    const intersectionPoints = [];
+
+    for (let i = 0; i < numLines - 1; i++) {
+        const line1 = lines[i];
+        const [x1, y1] = line1[0];
+        const [x2, y2] = line1[1];
+
+        for (let j = i + 1; j < numLines; j++) {
+            const line2 = lines[j];
+            const [x3, y3] = line2[0];
+            const [x4, y4] = line2[1];
+
+            const ua_num = (x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3);
+            const ua_denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+
+            const ub_num = (x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3);
+            const ub_denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+
+            const ua = ua_denom !== 0 ? ua_num / ua_denom : Number.MAX_VALUE;
+            const ub = ub_denom !== 0 ? ub_num / ub_denom : Number.MAX_VALUE;
+
+            if (ua >= 0 && ua <= 1 && ub >= 0 && ub <= 1) {
+                // Intersection found, calculate the point
+                const intersectionX = x1 + ua * (x2 - x1);
+                const intersectionY = y1 + ua * (y2 - y1);
+                intersectionPoints.push([intersectionX, intersectionY]);
+            }
+        }
+    }
+
+    visualizeIntersectionPoints(lines, intersectionPoints);
+
+    return intersectionPoints.length > 0 ? intersectionPoints : null; // No intersection found
+}
+
+function visualizeIntersectionPoints(lines, intersectionPoints) {
+    const svg = d3.select("#visualization");
+
+    // Draw intersection points
+    svg.selectAll("circle")
+        .data(intersectionPoints)
+        .enter().append("circle")
+        .attr("cx", d => d[0])
+        .attr("cy", d => d[1])
+        .attr("r", 5)
+        .attr("fill", "red");
+}
